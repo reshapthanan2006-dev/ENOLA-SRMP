ce2f8334e606408e33a1e20f395e78828c61e864
reshapthanan2006 <reshapthanan2006@gmail.com>
2026-09-08 13:16:06 +0530

    Member 1: add authentication controller

--- SRMP/Controllers/AuthController.cs
+++ SRMP/Controllers/AuthController.cs
@@ -1,6 +1,39 @@
-﻿namespace SRMP.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SRMP.DTOs;
+using SRMP.Interfaces;
+
+namespace SRMP.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(
+            [FromBody] RegisterDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+
+            return Ok(result);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(
+            [FromBody] LoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+
+            return Ok(result);
+        }
     }
 }
