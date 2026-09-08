22ae8189a7f0f6444f5d1fa54bb930f969b5409c
reshapthanan2006 <reshapthanan2006@gmail.com>
2026-09-08 12:46:14 +0530

    Member 1: implement authentication repository

--- SRMP/Repositories/AuthRepository.cs
+++ SRMP/Repositories/AuthRepository.cs
@@ -1,6 +1,37 @@
-﻿namespace SRMP.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using SRMP.Data;
+using SRMP.Interfaces;
+using SRMP.Models;
+
+namespace SRMP.Repositories
 {
-    public class AuthRepository
+    public class AuthRepository : IAuthRepository
     {
+        private readonly AppDbContext _context;
+
+        public AuthRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return await _context.Users
+                .AnyAsync(u => u.Email == email);
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
     }
 }
