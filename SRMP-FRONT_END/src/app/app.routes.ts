c08729d2025d881f8c48167b8e385211ae28fd73
reshapthanan2006 <reshapthanan2006@gmail.com>
2026-09-12 14:37:26 +0530

    Implement Member 1 authentication frontend

--- SRMP-FRONT_END/src/app/app.routes.ts
+++ SRMP-FRONT_END/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
