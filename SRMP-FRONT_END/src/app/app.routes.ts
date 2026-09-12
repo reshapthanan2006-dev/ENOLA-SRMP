82a57d2a3c07338a903971049552a6cc4e4dfc22
Stefani Mariachandrakumar <UT011143@unicomtic.com>
2026-09-12 15:01:06 +0530

    Add Member 3 company profile frontend

--- SRMP-FRONT_END/src/app/app.routes.ts
+++ SRMP-FRONT_END/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CompanyProfileComponent } from './features/employer/pages/company-profile/company-profile.component';
+
+export const routes: Routes = [
+  {
+    path: 'employer/company-profile',
+    component: CompanyProfileComponent
+  }
+];
